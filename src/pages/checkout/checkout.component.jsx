ddf9862188fe55ec8c26aa7eecbbe8b730add53f
COLIN COUTTS <COUTTSTCOLIN@GMAIL.COM>
2019-09-17 14:24:27 -0700

    built checkout item component

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -1,12 +1,14 @@
-import React from 'react';
-import { connect } from 'react-redux';
-import { createStructuredSelector } from 'reselect';
+import React from "react";
+import { connect } from "react-redux";
+import { createStructuredSelector } from "reselect";
+import CheckoutItem from "../../components/checkout-item/checkout-item.component";
+
 import {
   selectCartItems,
   selectCartTotal
-} from '../../redux/cart/cart.selectors';
+} from "../../redux/cart/cart.selectors";
 
-import './checkout.styles.scss';
+import "./checkout.styles.scss";
 
 const CheckoutPage = ({ cartItems, total }) => (
   <div className="checkout-page">
@@ -28,13 +30,11 @@ const CheckoutPage = ({ cartItems, total }) => (
       </div>
     </div>
 
-    {
-      cartItems.map(cartItem => 
-          cartItem.name
-        )
-    }
+    {cartItems.map(cartItem => (
+      <CheckoutItem key={cartItem.id} cartItem={cartItem} />
+    ))}
 
-    <div className='total'>
+    <div className="total">
       <span>TOTAL: ${total}</span>
     </div>
   </div>
