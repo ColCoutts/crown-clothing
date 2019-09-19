5a6968e06c45f3d38dbb40a423da4d33d13da73f
COLIN COUTTS <COUTTSTCOLIN@GMAIL.COM>
2019-09-19 16:32:08 -0700

    added stripe for payments and updated favicon and title

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import StripeCheckout from "react-stripe-checkout";
+
+const StripeCheckoutButton = ({ price }) => {
+  const priceForStripe = price * 100;
+  const publishableKey = "pk_test_eyEsoHO5wW5gey8G91a4bZjX00Pa9nqrKr";
+
+  const onToken = token => {
+    console.log(token);
+    alert("Payment successful");
+  };
+  return (
+    <StripeCheckout
+      label="Pay Now"
+      name="CRWN Clothing Ltd."
+      billingAddress
+      shippingAddress
+      image="https://svgshare.com/i/CUz.svg"
+      description={`Your total is ${price}`}
+      amount={priceForStripe}
+      panelLabel="Pay Now"
+      token={onToken}
+      stripeKey={publishableKey}
+    />
+  );
+};
+
+export default StripeCheckoutButton;
